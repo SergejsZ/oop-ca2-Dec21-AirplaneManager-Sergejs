0c6ee9aa66b557e882487d5d9ba840f9a9f2a3fe
serge <sergejzapivalovs@gmail.com>
2021-12-15 11:35:19 +0000

    Ca starter

--- src/main/java/dkit/oop/PassengerAirplane.java
+++ src/main/java/dkit/oop/PassengerAirplane.java
@@ -3,17 +3,38 @@ package dkit.oop;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PassengerAirplane  {
+public class PassengerAirplane  extends Airplane{
 
     // fields(Q2)
+    private int MAX_NUM_PASSENGERS;
+    private ArrayList <String> passengerList = new ArrayList<>();
 
-    PassengerAirplane(String type, int maxNumPassengers) {
-        // code here
+    public PassengerAirplane(String type, int MAX_NUM_PASSENGERS) {
+        super(type);
+        this.MAX_NUM_PASSENGERS = MAX_NUM_PASSENGERS;
+    }
 
+    public PassengerAirplane(String type, int MAX_NUM_PASSENGERS, ArrayList<String> passengerList) {
+        super(type);
+        this.MAX_NUM_PASSENGERS = MAX_NUM_PASSENGERS;
+        this.passengerList = passengerList;
+    }
+
+    @Override
+    public String toString() {
+        return "PassengerAirplane{" +
+                "MAX_NUM_PASSENGERS=" + MAX_NUM_PASSENGERS +
+                ", passengerList=" + passengerList +
+                '}' + super.toString();
     }
 
     public void addPassenger(String name) {
-        // code here
+        if(passengerList.size() < MAX_NUM_PASSENGERS) {
+            passengerList.add(name);
+        }
+        else{
+            System.out.println("Max number of passengers reached!");
+        }
 
     }
 
